9bc7a0e71328e74bbdd655e10f08e634c075d2b9
Akash Adhikari <akash_532998@yahoo.com>
2018-02-07 22:25:52 +0545

    commn model semi-final

--- communication/admin.py
+++ communication/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import SalesGroup, SubStage, Communication
+from .models import SalesGroup, SubStage, Communication, ContactPerson
 
 # Register your models here.
 
 admin.site.register(SalesGroup)
 admin.site.register(SubStage)
 admin.site.register(Communication)
+admin.site.register(ContactPerson)
--- communication/models.py
+++ communication/models.py
@@ -25,15 +25,34 @@ class SubStage(models.Model):
     def __str__(self):
         return "{} > {}".format(self.sales_group, self.name)
 
+class ContactPerson(models.Model):
+
+    name = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+MEDIUMS = (
+    ('Outbound Call', 'Outbound Call'),
+    ('Inbound Call', 'Inbound Call'),
+    ('Inbound Email', 'Inbound Email'),
+    ('Outbound Email', 'Outbound Email'),
+    ('SMS', 'SMS'),
+    ('Meeting', 'Meeting')
+)
+
 
 class Communication(models.Model):
 
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    contact_person = models.CharField(max_length=255, blank=False)
+    contact_person = models.ForeignKey(ContactPerson, on_delete=models.CASCADE)
+    medium = models.CharField(max_length=100, choices=MEDIUMS)
     sales_stage = models.ForeignKey(SubStage, on_delete=models.CASCADE)
+    set_reminder= models.DateTimeField(null=True, blank=True)
+    remarks = models.TextField(null=True, blank=True)
     fields = JSONField()
-    created = models.DateField(auto_now=True)
+    created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return "{} is contacted by {} at {} on {}"\
-            .format(self.client, self.contact_person,  self.sales_stage, self.created)
+        return "{} is contacted by {} through {} at {} on {}"\
+            .format(self.client, self.contact_person, self.medium, self.sales_stage, self.created)
